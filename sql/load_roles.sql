690d06e30cb880e5eae72f5e05bec55db6294a7b
maximka777 <maksim.borovskij@yandex.ru>
2017-03-03 15:15:01 +0300

    sql script for role adding

---
+++ sql/load_roles.sql
@@ -0,0 +1,6 @@
+INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('user', '2');
+INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('pupil', '4');
+INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('teacher', '6');
+INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('director_of_studies', '8');
+INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('director', '10');
+INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('admin', '12');
