41ebbd07926406805a01a1ed52a629c1db2a6223
maximka777 <maksim.borovskij@yandex.ru>
2017-03-12 20:36:23 +0300

    401 error from interceptor

--- server/src/main/java/school/journal/interceptor/AuthInterceptor.java
+++ server/src/main/java/school/journal/interceptor/AuthInterceptor.java
@@ -26,6 +26,7 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 //                req.setAttribute("user", user);
 //            }
 //        } catch (AuthException exc) {
+//            resp.sendError(401);
 //            return false;
 //        }
 //        return user != null;
