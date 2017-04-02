5d8f99832415fade5d3a92480ead5acae0c27091
zasamen <kardashvg@gmail.com>
2017-04-02 19:50:12 +0300

    DB model and SQL insert fixes

--- sql/load_roles.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('user', '2');
-INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('pupil', '4');
-INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('teacher', '6');
-INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('director_of_studies', '8');
-INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('director', '10');
-INSERT INTO `school_journal_db`.`role` (`name`, `level`) VALUES ('admin', '12');
