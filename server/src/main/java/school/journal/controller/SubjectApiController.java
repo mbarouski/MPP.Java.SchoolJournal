c484643532be32d6ed01c8b4e867fce8c9b91532
Dmitry <mr.blacky01@gmail.com>
2017-04-04 03:48:49 +0300

    Change Subject cintroler

--- server/src/main/java/school/journal/controller/SubjectApiController.java
+++ server/src/main/java/school/journal/controller/SubjectApiController.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 
 @Controller
 @RequestMapping(value = "/api/subjects")
-public class SubjectAPIController {
+public class SubjectAPIController extends BaseController<Subject>{
     private Logger LOGGER = Logger.getLogger(SubjectAPIController.class);
 
     @Autowired
@@ -29,81 +29,34 @@ public class SubjectAPIController {
     @ResponseBody
     public ResponseEntity get(HttpServletRequest req)
             throws ControllerException {
-        ResponseEntity resultResponse;
-        try{
-            LOGGER.info("get subject list controller method");
-            resultResponse = new ResponseEntity(subjectService.read(), HttpStatus.OK);
-        } catch (ServiceException exc){
-            resultResponse = new ResponseEntity(new ArrayList<>(), HttpStatus.OK);
-        } catch (Exception exc) {
-            LOGGER.error(exc);
-            resultResponse = new ResponseEntity(new ErrorObject("Some critical error"), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return resultResponse;
+        return read(() -> subjectService.read(), "Can't get full subjects list", LOGGER);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity create(@RequestBody Subject subject)
             throws ControllerException {
-        ResponseEntity resultResponse;
-        try{
-            resultResponse = new ResponseEntity(subjectService.create(subject), HttpStatus.OK);
-        } catch (ServiceException exc){
-            resultResponse = new ResponseEntity(new ErrorObject("Error in subject creating"), HttpStatus.BAD_REQUEST);
-        } catch (Exception exc) {
-            LOGGER.error(exc);
-            resultResponse = new ResponseEntity(new ErrorObject("Some critical error"), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return resultResponse;
+        return createOrUpdate((Subject s) -> subjectService.create(s), subject, "Can't create subject", LOGGER);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     @ResponseBody
     public ResponseEntity update(@RequestBody Subject subject)
             throws ControllerException {
-        ResponseEntity resultResponse;
-        try{
-            resultResponse = new ResponseEntity(subjectService.update(subject), HttpStatus.OK);
-        } catch (ServiceException exc){
-            resultResponse = new ResponseEntity(new ErrorObject("Error in subject updating"), HttpStatus.BAD_REQUEST);
-        } catch (Exception exc) {
-            LOGGER.error(exc);
-            resultResponse = new ResponseEntity(new ErrorObject("Some critical error"), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return resultResponse;
+        return createOrUpdate((Subject s) -> subjectService.update(s), subject, "Can't update subject", LOGGER);
     }
 
     @RequestMapping(value = "/{subjectId}", method = RequestMethod.DELETE)
     @ResponseBody
     public ResponseEntity delete(@PathVariable("subjectId") int subjectId)
             throws ControllerException {
-        ResponseEntity resultResponse;
-        try{
-            subjectService.delete(subjectId);
-            resultResponse = new ResponseEntity(HttpStatus.OK);
-        } catch (ServiceException exc){
-            resultResponse = new ResponseEntity(new ErrorObject("Error in subject deleting"), HttpStatus.BAD_REQUEST);
-        } catch (Exception exc) {
-            LOGGER.error(exc);
-            resultResponse = new ResponseEntity(new ErrorObject("Some critical error"), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return resultResponse;
+        return delete((int id) -> subjectService.delete(id), subjectId, "Can't delete subject by id", LOGGER);
     }
 
     @RequestMapping(value = "/{subjectId}")
     @ResponseBody
     public ResponseEntity getOne(@PathVariable("subjectId") int subjectId)
             throws ControllerException {
-        ResponseEntity resultResponse;
-        try{
-            resultResponse = new ResponseEntity(subjectService.getOne(subjectId), HttpStatus.OK);
-        } catch (ServiceException exc){
-            resultResponse = new ResponseEntity(new ErrorObject("Error in subject getting"), HttpStatus.BAD_REQUEST);
-        } catch (Exception exc) {
-            LOGGER.error(exc);
-            resultResponse = new ResponseEntity(new ErrorObject("Some critical error"), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return resultResponse;
+        return getOne((int id) -> subjectService.getOne(subjectId), subjectId, "Can't get subject by id", LOGGER);
     }
 }
