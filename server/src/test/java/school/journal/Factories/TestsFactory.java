6978bb96039795c99c475b19f9c5834f99faec00
Dmitry <mr.blacky01@gmail.com>
2017-04-21 14:21:06 +0300

    Add more repository tests

---
+++ server/src/test/java/school/journal/Factories/TestsFactory.java
@@ -0,0 +1,97 @@
+package school.journal.Factories;
+
+import org.hibernate.*;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.mockito.Mockito;
+import school.journal.repository.IRepository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class TestsFactory<T> extends BaseFactory {
+
+    final Class<T> typeParameterClass;
+
+    public  TestsFactory(Class<T> typeParameterClass) {
+        this.typeParameterClass = typeParameterClass;
+    }
+    @Override
+    public SessionFactory createMockSessionFactory(Session session) {
+        SessionFactory result = createSessionFactory();
+        when(result.openSession()).thenReturn((org.hibernate.classic.Session) session);
+        return result;
+    }
+
+    @Override
+    public Session createMockSession(){
+        Session result = Mockito.mock(Session.class);
+        when(result.beginTransaction()).thenReturn(Mockito.mock(Transaction.class));
+        return result;
+    }
+
+    @Override
+    public Session createMockSessionWithException() {
+        Session session = createMockSession();
+        doThrow(HibernateException.class).when(session).get(any(Class.class),anyInt());
+        doThrow(HibernateException.class).when(session).save(any(Object.class));
+        doThrow(HibernateException.class).when(session).update(any(Object.class));
+        doThrow(HibernateException.class).when(session).delete(any(Object.class));
+        doThrow(HibernateException.class).when(session).createCriteria(any(Class.class));
+        doThrow(HibernateException.class).when(session).merge(any(Object.class));
+        return session;
+    }
+
+    @Override
+    public Session createMockSessionWithGetAll(List expectedObjects) {
+        Session session = createMockSession();
+        Criteria mockCriteria = Mockito.mock(Criteria.class);
+        when(mockCriteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)).thenReturn(mockCriteria);
+        when(mockCriteria.list()).thenReturn(expectedObjects);
+        when(session.createCriteria(any(Class.class))).thenReturn(mockCriteria);
+        when(mockCriteria.addOrder(any(Order.class))).thenReturn(mockCriteria);
+        return session;
+    }
+
+    @Override
+    public Session createMockSessionWithGet(Object returnedObject) {
+        Session session = createMockSession();
+        when(session.get(any(Class.class),anyInt())).thenReturn(returnedObject);
+        return session;
+    }
+
+    @Override
+    public Session createMockSessionWithSave() {
+        Session session = createMockSession();
+        when(session.save(typeParameterClass)).thenReturn(anyInt());
+        return session;
+    }
+
+    @Override
+    public Session createMockSessionWithUpdate() {
+        Session session = createMockSession();
+        //when(session.update(typeParameterClass)).thenReturn();
+        return session;
+    }
+
+    @Override
+    public Session createMockSessionWithGetByName(Object deletedObject, String name) {
+        Session session = createMockSession();
+        Criteria mockCriteria = Mockito.mock(Criteria.class);
+        when(mockCriteria.add(Restrictions.eq(name, anyString()))).thenReturn(mockCriteria);
+        when(mockCriteria.uniqueResult()).thenReturn(deletedObject);
+        when(session.createCriteria(typeParameterClass)).thenReturn(mockCriteria);
+        return session;
+    }
+
+    @Override
+    public IRepository createMockContactDataRepository() {
+        return Mockito.mock(IRepository.class);
+    }
+
+    private SessionFactory createSessionFactory(){
+        return Mockito.mock(SessionFactory.class);
+    }
+}
---
+++ server/src/test/java/school/journal/ValidateServiceUtilsTest.java
@@ -0,0 +1,86 @@
+package school.journal;
+
+
+import org.junit.Test;
+import school.journal.utils.exception.ValidationException;
+
+import java.util.Date;
+
+import static school.journal.utils.ValidateServiceUtils.*;
+
+
+public class ValidateServiceUtilsTest {
+
+    @Test(expected = ValidationException.class)
+    public void testValidateId_withBellowZeroValues() throws Exception{
+        validateId(-1,"");
+        validateId(0,"");
+    }
+
+    @Test
+    public void testValidateId_withCorrect() throws Exception{
+        validateId(1,"");
+        validateId(100,"");
+    }
+
+    @Test
+    public void testValidateNullableString_WithCorrectValue() throws Exception{
+        validateNullableString("no","");
+
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateNullableString_withEmptyString() throws Exception{
+        validateNullableString("","");
+    }
+
+    @Test
+    public void testValidateString_WithCorrectValue() throws Exception{
+        validateString("no","");
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateString_withNull() throws Exception{
+        validateString(null,"");
+    }
+
+    @Test
+    public void testValidatePhone_WithCorrectValue() throws Exception{
+        validatePhone("+375291111111");
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidatePhone_withEmptyString() throws Exception{
+        validatePhone("");
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidatePhone_withIncorrectString() throws Exception{
+        validatePhone("abs");
+    }
+
+    @Test
+    public void testValidateEmail_WithCorrectValue() throws Exception{
+        validateEmail("abc@g");
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateEmail_withEmptyString() throws Exception{
+        validateEmail("");
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateEmail_withIncorrectString() throws Exception{
+        validateEmail("abs");
+    }
+
+    @Test
+    public void testValidateDatePeriod_WithCorrectValue() throws Exception{
+        validateDatePeriod(new Date(123),new Date(124));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void testValidateDatePeriod_withIncorrectString() throws Exception{
+        validateDatePeriod(new Date(125),new Date(124));
+    }
+}
