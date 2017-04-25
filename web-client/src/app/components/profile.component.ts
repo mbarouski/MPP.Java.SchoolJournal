6eef48981ecbe36b72c34f6297dfe018f294d63b
maximka777 <maksim.borovskij@yandex.ru>
2017-04-26 00:03:33 +0300

    profile info validation

--- web-client/src/app/components/profile.component.ts
+++ web-client/src/app/components/profile.component.ts
@@ -25,6 +25,26 @@ export class ProfileComponent implements AfterViewInit, OnInit {
       status: false,
       message: '',
     },
+    firstName: {
+      status: false,
+      message: '',
+    },
+    pathronymic: {
+      status: false,
+      message: '',
+    },
+    lastName: {
+      status: false,
+      message: '',
+    },
+    phoneNumber: {
+      status: false,
+      message: '',
+    },
+    description: {
+      status: false,
+      message: '',
+    },
   };
 
   errorMessage = '';
@@ -110,11 +130,81 @@ export class ProfileComponent implements AfterViewInit, OnInit {
   }
 
   validateProfileData() {
+    this.validateFirstName();
+    this.validatePathronymic();
+    this.validateLastName();
+    this.validatePhoneNumber();
+    this.validateDescription();
+  }
 
+  validateDescription() {
+    const description = this.currentTeacherPupil.description || this.currentTeacherPupil.characteristic;
+    if(!description) {
+      this.validationError.description.status = true;
+      this.validationError.description.message = 'Введите описание';
+    } else {
+      this.validationError.description.status = false;
+    }
+  }
+
+  validatePhoneNumber() {
+    const phoneNumber = this.currentTeacherPupil.phoneNumber;
+    if(!phoneNumber) {
+      this.validationError.phoneNumber.status = true;
+      this.validationError.phoneNumber.message = 'Введите номер телефона';
+    } else if(!/^\+375[0-9]{9}$/g.test(phoneNumber)) {
+      this.validationError.phoneNumber.status = true;
+      this.validationError.phoneNumber.message = 'Номер телефона должен быть записан в формате "+375XXXXXXXXX"';
+    } else {
+      this.validationError.phoneNumber.status = false;
+    }
+  }
+
+  validateFirstName() {
+    const firstName = this.currentTeacherPupil.firstName;
+    if(!firstName) {
+      this.validationError.firstName.status = true;
+      this.validationError.firstName.message = 'Введите имя';
+    } else if(!/^[А-Яа-я-]+$/g.test(firstName)) {
+      this.validationError.firstName.status = true;
+      this.validationError.firstName.message = 'Имя должно содержать только кириллические символы и знак тире "-"';
+    } else {
+      this.validationError.firstName.status = false;
+    }
+  }
+
+  validateLastName() {
+    const lastName = this.currentTeacherPupil.lastName;
+    if(!lastName) {
+      this.validationError.lastName.status = true;
+      this.validationError.lastName.message = 'Введите фамилию';
+    } else if(!/^[А-Яа-я-]+$/g.test(lastName)) {
+      this.validationError.lastName.status = true;
+      this.validationError.lastName.message = 'Фамилия должна содержать только кириллические символы и знак тире "-"';
+    } else {
+      this.validationError.lastName.status = false;
+    }
+  }
+
+  validatePathronymic() {
+    const pathronymic = this.currentTeacherPupil.pathronymic;
+    if(!pathronymic) {
+      this.validationError.pathronymic.status = true;
+      this.validationError.pathronymic.message = 'Введите отчество';
+    } else if(!/^[А-Яа-я-]+$/g.test(pathronymic)) {
+      this.validationError.pathronymic.status = true;
+      this.validationError.pathronymic.message = 'Отчество должно содержать только кириллические символы и знак тире "-"';
+    } else {
+      this.validationError.pathronymic.status = false;
+    }
   }
 
   isProfileDataValid() {
-
+    return !this.validationError.firstName.status &&
+      !this.validationError.pathronymic.status &&
+      !this.validationError.lastName.status &&
+      !this.validationError.phoneNumber.status &&
+      !this.validationError.description.status;
   }
 
   openPasswordModal() {
@@ -142,17 +232,18 @@ export class ProfileComponent implements AfterViewInit, OnInit {
   openEditModal() {
     let personInfo = this.user.role == 'pupil' ? this.pupil : this.teacher;
     this.currentTeacherPupil = new TeacherPupil(this.user.userId);
-    // // this.currentTeacherPupil.classId = personInfo.classId;
-    // this.currentTeacherPupil.firstName = personInfo.firstName;
-    // this.currentTeacherPupil.pathronymic = personInfo.pathronymic;
-    // this.currentTeacherPupil.lastName = personInfo.lastName;
-    // this.currentTeacherPupil.phoneNumber = personInfo.phoneNumber;
-    // this.currentTeacherPupil.description = personInfo.description;
-    // this.currentTeacherPupil.characteristic = personInfo.characteristic;
+    this.currentTeacherPupil.firstName = personInfo.firstName;
+    this.currentTeacherPupil.pathronymic = personInfo.pathronymic;
+    this.currentTeacherPupil.lastName = personInfo.lastName;
+    this.currentTeacherPupil.phoneNumber = personInfo.phoneNumber;
+    this.currentTeacherPupil.description = personInfo.description;
+    this.currentTeacherPupil.characteristic = personInfo.characteristic;
     this.editModal.show();
   }
 
   onEditFormSubmit() {
+    this.validateProfileData();
+    if(!this.isProfileDataValid()) return;
     let serviceMethod = this.user.role.name === 'pupil'
       ? this.pupilsService.updatePupil.bind(this.pupilsService)
       : this.teachersService.updateTeacher.bind(this.teachersService);
